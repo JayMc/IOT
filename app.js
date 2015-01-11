24f2d0101b8283f593e2001ceddd51f095f72e7f
Jason McIver <mciver.jason@gmail.com>
2015-01-11 17:55:11 +1100

    Init

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+    console.log(req.body)
+    res.sendStatus(200);
+});
+
+
+var server = app.listen(7777, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Listening at http://%s:%s', host, port)
+});
+
